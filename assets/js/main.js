d5f7a210b54951a040fe435e7c6b0b4ea0d0e216
MauryaRitesh <ritesh.k.m786@gmail.com>
2021-06-27 17:21:15 +0530

    f`_commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,21 +96,41 @@ modalCloses.forEach((modalClose) =>{
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper('.portfolio-container', {
+let swiperPortfolio = new Swiper('.portfolio__container', {
     cssMode: true,
+    loop:true,
+
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
     pagination: {
-        el: '.swiper-pagination'
+        el: '.swiper-pagination',
+        clickable:true,
     },
-    mousewheel: true,
-    keyboard: true,
 });
 
 /*==================== TESTIMONIAL ====================*/
+let swiperTestimonial = new Swiper('.testimonial__container', {
+    loop:true,
+    grabCursor:true,
+    spaceBetween: 48,
 
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable:true,
+        dynamicBullets:true,
+    },
+    breakpoints:{
+        568:{
+            slidesPerView:2,
+        }
+    }
+});
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
